0e69d9919a271448550a3132e8be65c507487fff
David Inyang-Etoh <dinyangetoh@gmail.com>
2019-01-30 10:44:40 +0100

    update dependencies

--- app.js
+++ app.js
@@ -13,23 +13,34 @@ var api_routes = require('./app/routes/api');
 var web_routes = require('./app/routes/web');
 
 // Configure bodyparser to handle post requests
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
 // Setup server port
 var port = process.env.PORT || 8080;
 
 // Frontend configurations for template engine
-app.engine('.hbs', handlebars.engine, exphbs({ defaultLayout: 'layout', extname: '.hbs', layoutsDir: './web/views/layouts' }));
+app.engine('.hbs', handlebars.engine, exphbs({
+    defaultLayout: 'layout',
+    extname: '.hbs',
+    layoutsDir: './web/views/layouts'
+}));
 app.set('view engine', '.hbs');
 // Set directory for template views
 app.set('views', __dirname + '/web/views/');
-app.set('view options', { layout: 'layout' });
+app.set('view options', {
+    layout: 'layout'
+});
 
 
 mongoose.Promise = global.Promise;
 // Connect to Mongoose and set connection variable
-mongoose.connect('mongodb://heroku_dbadmin:nwl6b0kq@ds227525.mlab.com:27525/heroku_nwl6b0kq', { useMongoClient: true });
+mongoose.connect('mongodb://localhost/sis', {
+    useMongoClient: true
+});
+// mongoose.connect('mongodb://heroku_dbadmin:nwl6b0kq@ds227525.mlab.com:27525/heroku_nwl6b0kq', { useMongoClient: true });
 var db = mongoose.connection;
 
 // API routes
@@ -38,6 +49,6 @@ app.use('/api', api_routes);
 app.use('/', web_routes);
 
 
-app.listen(port, function() {
+app.listen(port, function () {
     console.log("Running on port " + port);
 });
